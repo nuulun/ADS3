123b4ce5e846ce77caf4d114c6741c659cc1d463
nurlan <bekovn91@gmail.com>
2025-04-29 18:41:49 +0500

    first commit

--- src/Tree.java
+++ src/Tree.java
@@ -12,6 +12,49 @@ public class Tree<K extends Comparable<K>, V> {
         }
     }
     public void put(K key, V val) {
+        root = put(root, key, val);
+    }
 
+    private Node put(Node x, K key, V val) {
+        if (x == null) {
+            size++;
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        return x;
+    }
+
+    public V get(K key) {
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return x.val;
+        }
+        return null;
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, K key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            size--;
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+
+            Node t = x;
+
+        }
+        return x;
     }
 }
